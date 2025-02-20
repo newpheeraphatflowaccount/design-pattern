9ff36087076f00266736bd8994e142d30e3cfd53
newpheeraphatflowaccount <165787650+newpheeraphatflowaccount@users.noreply.github.com>
2025-02-20 23:51:29 +0700

    feat: add FluentBuilder with Recursive Generics

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -2,74 +2,6 @@
 
 namespace Builder
 {
-	public class HtmlElement
-	{
-		public string Name, Text;
-		public List<HtmlElement> Elements = new List<HtmlElement>();
-		private const int indentSize = 2;
-
-		public HtmlElement()
-		{
-		}
-
-		public HtmlElement(string name, string text)
-		{
-			Name = name;
-			Text = text;
-		}
-
-		private string ToStringImpl(int indent)
-		{
-			var sb = new StringBuilder();
-			var i = new string(' ', indentSize * indent);
-			sb.Append($"{i}<{Name}>\n");
-			if (!string.IsNullOrWhiteSpace(Text))
-			{
-				sb.Append(new string(' ', indentSize * (indent + 1)));
-				sb.Append(Text);
-				sb.Append("\n");
-			}
-
-			foreach (var e in Elements)
-				sb.Append(e.ToStringImpl(indent + 1));
-
-			sb.Append($"{i}</{Name}>\n");
-			return sb.ToString();
-		}
-
-		public override string ToString()
-		{
-			return ToStringImpl(0);
-		}
-	}
-
-	public class HtmlBuilder
-	{
-		private readonly string rootName;
-		HtmlElement root = new HtmlElement();
-		public HtmlBuilder(string rootName)
-		{
-			this.rootName = rootName;
-			root.Name = rootName;
-		}
-
-		public void AddChild(string childName, string childText)
-		{
-			var e = new HtmlElement(childName, childText);
-			root.Elements.Add(e);
-		}
-
-		public override string ToString()
-		{
-			return root.ToString();
-		}
-
-		public void Clear()
-		{
-			root = new HtmlElement { Name = rootName };
-		}
-	}
-
 	class Program
 	{
 		static void Main(string[] args)
@@ -91,9 +23,9 @@ namespace Builder
 			sb.Append("</ul>");
 			Console.WriteLine(sb.ToString());
 
-			var builder = new HtmlBuilder("ul");
-			builder.AddChild("li", "hello");
-			builder.AddChild("li", "world");
+			var builder = new HtmlBuilder("ul")
+				.AddChild("li", "hello")
+				.AddChild("li", "world");
 			Console.WriteLine(builder.ToString());
 		}
 	}
---
+++ FluentBuilder/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace FluentBuilder
+{
+	public class Person
+	{
+		public string Name;
+		public string Position;
+
+		public class Builder : PersonJobBuilder<Builder>
+		{
+		}
+
+		public static Builder New => new Builder();
+
+		public override string ToString()
+		{
+			return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+		}
+	}
+
+	public abstract class PersonBuilder
+	{
+		protected Person person = new Person();
+		public Person Build()
+		{
+			return person;
+		}
+	}
+
+	// class Foo : Bar<Foo>
+	public class PeronInfoBuilder<SELF>
+		: PersonBuilder
+		where SELF : PeronInfoBuilder<SELF>
+	{
+		public SELF Called(string name)
+		{
+			person.Name = name;
+			return (SELF) this;
+		}
+	}
+
+	public class PersonJobBuilder<SELF>
+		: PeronInfoBuilder<PersonJobBuilder<SELF>>
+		where SELF : PersonJobBuilder<SELF>
+	{
+		public SELF WorksAsA(string position)
+		{
+			person.Position = position;
+			return (SELF) this;
+		}
+	}
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var me = Person.New
+				.Called("Dmitri")
+				.WorksAsA("Quant")
+				.Build();
+			System.Console.WriteLine(me);
+		}
+	}
+}
