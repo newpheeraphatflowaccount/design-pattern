2cf386ef67cc3eb2694e02c7abfff489d4cf8d98
newpheeraphatflowaccount <165787650+newpheeraphatflowaccount@users.noreply.github.com>
2025-02-21 00:16:58 +0700

    feat: add fluent builder again

--- FluentBuilder/Program.cs
+++ FluentBuilder/Program.cs
@@ -1,12 +1,20 @@
-﻿namespace FluentBuilder
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace DesignPatterns
 {
 	public class Person
 	{
 		public string Name;
+
 		public string Position;
 
-		public class Builder : PersonJobBuilder<Builder>
+		public DateTime DateOfBirth;
+
+		public class Builder : PersonBirthDateBuilder<Builder>
 		{
+			internal Builder() { }
 		}
 
 		public static Builder New => new Builder();
@@ -20,44 +28,63 @@
 	public abstract class PersonBuilder
 	{
 		protected Person person = new Person();
+
 		public Person Build()
 		{
 			return person;
 		}
 	}
 
-	// class Foo : Bar<Foo>
-	public class PeronInfoBuilder<SELF>
-		: PersonBuilder
-		where SELF : PeronInfoBuilder<SELF>
+	public class PersonInfoBuilder<SELF> : PersonBuilder
+	  where SELF : PersonInfoBuilder<SELF>
 	{
 		public SELF Called(string name)
 		{
 			person.Name = name;
-			return (SELF) this;
+			return (SELF)this;
 		}
 	}
 
 	public class PersonJobBuilder<SELF>
-		: PeronInfoBuilder<PersonJobBuilder<SELF>>
-		where SELF : PersonJobBuilder<SELF>
+	  : PersonInfoBuilder<PersonJobBuilder<SELF>>
+	  where SELF : PersonJobBuilder<SELF>
 	{
 		public SELF WorksAsA(string position)
 		{
 			person.Position = position;
-			return (SELF) this;
+			return (SELF)this;
 		}
 	}
 
-	public class Program
+	// here's another inheritance level
+	// note there's no PersonInfoBuilder<PersonJobBuilder<PersonBirthDateBuilder<SELF>>>!
+
+	public class PersonBirthDateBuilder<SELF>
+	  : PersonJobBuilder<PersonBirthDateBuilder<SELF>>
+	  where SELF : PersonBirthDateBuilder<SELF>
 	{
+		public SELF Born(DateTime dateOfBirth)
+		{
+			person.DateOfBirth = dateOfBirth;
+			return (SELF)this;
+		}
+	}
+
+	internal class Program
+	{
+		class SomeBuilder : PersonBirthDateBuilder<SomeBuilder>
+		{
+
+		}
+
 		public static void Main(string[] args)
 		{
 			var me = Person.New
-				.Called("Dmitri")
-				.WorksAsA("Quant")
-				.Build();
-			System.Console.WriteLine(me);
+			  .Called("Dmitri")
+			  .WorksAsA("Quant")
+			  .Born(DateTime.UtcNow)
+			  .Build();
+			Console.WriteLine(me);
 		}
 	}
 }
